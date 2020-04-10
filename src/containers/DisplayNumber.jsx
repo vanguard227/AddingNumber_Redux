f336c694b381cd5234730164faa1d7c603266966
Vanguard <vanguard227@protonmail.com>
2020-04-11 06:32:35 +0800

    Add Display Container Component

---
+++ src/containers/DisplayNumber.jsx
@@ -0,0 +1,16 @@
+import DisplayNumber from '../component/DisplayNumber'
+import React, { Component } from 'react';
+import store from '../store';
+
+export default class extends Component {
+    state = {number:store.getState().number}
+    constructor(props) {
+        super(props);
+        store.subscribe(function(){
+        this.setState({number:store.getState().number});
+        }.bind(this))
+    }
+    render() {
+        return <DisplayNumber number={this.state.number}></DisplayNumber>
+    }
+}
